45d67c9bf3185371cb7657ed958f5627526bda85
shchansky <gavrilenko71285@gmail.com>
2023-01-16 19:07:48 +0300

    cmmit

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,37 @@ import React from "react";
 import "./App.css";
 
 function App() {
+  const [todos, setTodos] = React.useState<string[]>([]);
+  const [text, setText] = React.useState<string>("");
 
-  const [todos, setTodos]= React.useState<string[]>([])
-  const [text, setText] = React.useState<string>("")
+  const handleAddTodo = React.useCallback(() => {
+    if (text.trim().length) {
+      setTodos([...todos, text]);
+      setText("");
+    }
+  }, [text, todos]);
 
-  
+  const handleSetText = React.useCallback(
+    (ev: React.ChangeEvent<HTMLInputElement>) => {
+      setText(ev.target.value);
+    },
+    []
+  );
 
-
-  return <div className="App">
-
-
-
-  </div>;
+  return (
+    <div>
+      <div>
+        <input type="text" value={text} onChange={handleSetText} />
+        <button onClick={handleAddTodo}>setTodos</button>
+      </div>
+      <hr />
+      <div>
+        {todos.map((el, index) => (
+          <div key={index}>{el}</div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
