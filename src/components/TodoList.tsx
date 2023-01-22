8bdec5ba9d5bf2d25c01b19a97862e7fd9bde85a
shchansky <gavrilenko71285@gmail.com>
2023-01-22 10:35:01 +0300

    commit

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,37 +1,3 @@
-// import React from "react";
-// import { TodoItem } from "./TodoItem";
-
-// type TodoItemType = {
-//   id: string;
-//   text: string;
-//   completed: boolean;
-// };
-
-// type Props = {
-//   todos: TodoItemType[];
-//   handleToggleTodoCompleet: (id: string) => void;
-//   handleRemoveTodo: (id: string) => void;
-// };
-
-// export const TodoList = (props: Props) => {
-//   const { todos, handleToggleTodoCompleet, handleRemoveTodo } = props;
-
-//   return (
-//     <ul>
-//       {todos.map((todo) => (
-//         <TodoItem
-//           key={todo.id}
-//           id={todo.id}
-//           completed={todo.completed}
-//           text={todo.text}
-//           handleRemoveTodo={handleRemoveTodo}
-//           handleToggleTodoCompleet={handleToggleTodoCompleet}
-//         />
-//       ))}
-//     </ul>
-//   );
-// };
-
 import { useAppSelector } from "../hooks";
 import TodoItem from "./TodoItem";
 
