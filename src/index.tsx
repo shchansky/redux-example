93c5b4d2c172460e3e8f5bacd86ad0d841ec9749
shchansky <gavrilenko71285@gmail.com>
2023-01-27 21:13:39 +0300

    commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import App from "./app";
+import App from "./App";
 import store from "./store";
 
 ReactDOM.render(
